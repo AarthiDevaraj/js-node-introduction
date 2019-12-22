796cb226b11cca672c6498d7c5e9da4cf600fb85
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 15:49:07 +0530

    comments

--- 09-http-exercise-solution.js
+++ 09-http-exercise-solution.js
@@ -6,7 +6,9 @@ let readFilePromise = util.promisify(fs.readFile);
 
 http
   .createServer(async (req, res) => {
+    //    wait till the file is read
     const content = await readFilePromise('index.html', { encoding: 'utf-8' });
+    //  write to the response stream
     res.write(content);
     res.end();
   })
